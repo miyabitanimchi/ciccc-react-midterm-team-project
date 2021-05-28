f096fa1d4de9cea1f6e52ab2ba0668ce0c14b44d
Taichi I <fng.trf.kk1020@gmail.com>
2021-05-28 12:00:28 -0700

    fixed bug about handling asynchronous process

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useProductsContext } from '../context/products-context';
 
 function App() {
+  const { products, setProducts } = useProductsContext();
+
   return (
     <div className="App">
       <div>This is App component</div>
+      <div>products: {products.length !== 0 && products[0].category}</div>
     </div>
   );
 }
