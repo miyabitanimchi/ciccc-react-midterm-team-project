d7f06c2a307060b7bc645427a543875fcb6b019c
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-05-28 01:55:40 -0700

    add detail/cart folders/files, modifiy Detail.js

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from '../components/App';
-import Detail from '../components/Detail';
-import Cart from '../components/Cart';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "../components/App";
+import Detail from "../components/detail/Detail";
+import Cart from "../components/cart/Cart";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Header />
     <Switch>
-      <Route path='/' component={App} exact={true} />
-      <Route path='/detail/:id' component={Detail} />
-      <Route path='/cart' component={Cart} />
+      <Route path="/" component={App} exact={true} />
+      <Route path="/detail/:id" component={Detail} />
+      <Route path="/cart" component={Cart} />
     </Switch>
     <Footer />
+    {/* <Detail /> */}
   </BrowserRouter>
-)
+);
 
 export default AppRouter;
