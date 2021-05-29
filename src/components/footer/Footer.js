7fc936780acfb92bb95ed9e4e4428f18b3871655
kit486759 <kit486759@gmail.com>
2021-05-28 22:52:45 -0700

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD:src/components/Footer.js
-import React from 'react';
-import './scss/Footer.scss';
 
-const Footer = () => {
-  return (
-    <>
-      <footer>This is Footer.</footer>
-    </>
-  );
-}
-
-export default Footer;
-=======
 import React from 'react';
 import './Footer.scss';
 
@@ -24,4 +11,4 @@ const Footer = () => {
 }
 
 export default Footer;
->>>>>>> f61c26755e24decad6d8555507325b57c60f1cf5:src/components/footer/Footer.js
+
