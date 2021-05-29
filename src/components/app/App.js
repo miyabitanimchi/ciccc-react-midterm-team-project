f61c26755e24decad6d8555507325b57c60f1cf5
Taichi I <fng.trf.kk1020@gmail.com>
2021-05-28 20:49:42 -0700

    fixed conflict

---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useProductsContext } from '../../context/products-context';
+import Carousel from '../carousel/Carousel'
+import './App.scss';
+
+function App() {
+  const { products } = useProductsContext();
+
+  return (
+    <div className="App">
+        <div>
+          <div>This is App component</div>
+          { products.length !== 0 && (
+            <div>
+              <div>products: {products[0].title}</div>
+              <Carousel item={products}/>
+            </div>
+          )
+          }
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
