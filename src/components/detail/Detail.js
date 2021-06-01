d167078c088fe62f17d1bf9b068eac2e3f78d5d4
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-05-31 21:06:44 -0700

    modify localStorage function etc

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import { useProductsContext } from "../../context/products-context";
+
 import "./Detail.scss";
 
+const hello = JSON.parse(localStorage.getItem("addedProductsArr"));
+
 const Detail = (props) => {
-  props = 1;
   const { products } = useProductsContext();
   const [chosenProductInfo, setchosenProductInfo] = useState({
     product: [],
@@ -12,9 +14,12 @@ const Detail = (props) => {
     color: "",
     quantity: 1,
   });
+  const [addedProductsArr, setAddedProductsArr] = useState(hello || []);
 
   const filterChosenProduct = () => {
-    const chosenProduct = products.filter((product) => product.id === props);
+    const chosenProduct = products.filter(
+      (product) => product.id === Number(props.match.params.id)
+    );
     setchosenProductInfo({ ...chosenProductInfo, product: chosenProduct });
   };
 
@@ -40,16 +45,16 @@ const Detail = (props) => {
   const addToCart = () => {
     // for loaclStorage
     console.log("add cart is clicked");
-    // 今とりあえずこう書いとく
-    const productsAddedToCart = [];
-    // productsAddedToCart.pop(chosenProductInfo);
-    productsAddedToCart.push(chosenProductInfo);
-    localStorage.setItem(
-      "addedProductsArr",
-      JSON.stringify(productsAddedToCart)
-    );
-    console.log(productsAddedToCart);
+    setAddedProductsArr((addedProductsArr) => {
+      return [...addedProductsArr, chosenProductInfo];
+    });
+    console.log(chosenProductInfo);
   };
+
+  useEffect(() => {
+    localStorage.setItem("addedProductsArr", JSON.stringify(addedProductsArr));
+  }, [addedProductsArr]);
+  console.log(addedProductsArr);
 
   return (
     <main>
---
+++ src/components/detail/Specification.js
@@ -0,0 +1,98 @@
+// import "./Detail.scss";
+
+// const Specification = (props) => {
+//   return (
+//     <>
+//       <p className="size-title">
+//         Size: <span>{chosenProductInfo.size}</span>
+//       </p>
+//       <div className="size-wrap">
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="XXS"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="XS"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="S"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="M"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="L"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="XL"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//         <input
+//           className="size-btn"
+//           type="submit"
+//           value="XXL"
+//           onClick={(e) => showChosenSize(e.target)}
+//         />
+//       </div>
+//       <p className="color-title">
+//         Colour: <span>{chosenProductInfo.color}</span>
+//       </p>
+//       <div className="color-wrap">
+//         <input
+//           className="color-btn beige"
+//           type="submit"
+//           value="Beige"
+//           onClick={(e) => showChosenColor(e.target)}
+//         />
+//         <input
+//           className="color-btn navy"
+//           type="submit"
+//           value="Navy"
+//           onClick={(e) => showChosenColor(e.target)}
+//         />
+//         <input
+//           className="color-btn gray"
+//           type="submit"
+//           value="Gray"
+//           onClick={(e) => showChosenColor(e.target)}
+//         />
+//         <input
+//           className="color-btn black"
+//           type="submit"
+//           value="Black"
+//           onClick={(e) => showChosenColor(e.target)}
+//         />
+//         <input
+//           className="color-btn brown"
+//           type="submit"
+//           value="Brown"
+//           onClick={(e) => showChosenColor(e.target)}
+//         />
+//         <input
+//           className="color-btn off-white"
+//           type="submit"
+//           value="Off-white"
+//           onClick={(e) => showChosenColor(e.target)}
+//         />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Specification;
