a69c6cd69643d8c81502f57bdbcea1ba7d36e3df
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-17 01:20:08 -0700

    add quantity selector

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import "./CartItem.scss";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { useStyles } from "./style-materialUi";
 
 // child
 
@@ -16,6 +22,12 @@ const CartItem = (props) => {
     getNewAddedProductsArr,
     user,
   } = props;
+  const [testQty, setTestQty] = useState(quantity);
+  const classes = useStyles();
+
+  const handleChange = (event) => {
+    setTestQty(event.target.value);
+  };
 
   const removeProduct = (id) => {
     getNewAddedProductsArr(id);
@@ -30,13 +42,39 @@ const CartItem = (props) => {
           <h3 className="title">{product[0].title}</h3>
           <p className="size">Size: {size}</p>
           <p className="color">Colour: {color}</p>
-          <p className="quantity">Quantity: {quantity}</p>
+          {/* <p className="quantity">Quantity: {quantity}</p> */}
           <p className="unitPrice">
             CAD $ {(Math.round(product[0].price * 10) / 10).toFixed(2)}
           </p>
           <p className="subtotal">
             Subtotal: CAD ${Number(subTotal).toFixed(2)}
           </p>
+          <FormControl className={classes.formControl}>
+            <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+              Quantity
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-placeholder-label-label"
+              id="demo-simple-select-placeholder-label"
+              value={testQty}
+              onChange={handleChange}
+              displayEmpty
+              className={classes.selectEmpty}
+            >
+              <MenuItem value={quantity}>{quantity}</MenuItem>
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={6}>6</MenuItem>
+              <MenuItem value={7}>7</MenuItem>
+              <MenuItem value={8}>8</MenuItem>
+              <MenuItem value={9}>9</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+            </Select>
+            <FormHelperText>Label + placeholder</FormHelperText>
+          </FormControl>
         </div>
         <IoMdClose
           className="close-icon"
