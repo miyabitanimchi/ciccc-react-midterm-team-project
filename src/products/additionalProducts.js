7a91f2e9f4c058ec9c90e05986d69a2adb8726d4
kit486759 <kit486759@gmail.com>
2021-09-05 11:13:26 -0700

    -a

--- src/products/additionalProducts.js
+++ src/products/additionalProducts.js
@@ -1,13 +1,79 @@
 const categoryArr = ['clothes', 'accessories', 'electronics'];
 
 // You can add any items here!
-const additionalProducts = [{
-  id: 21,
-  title: "Swotch || Mintendo",
-  price: 389.99,
-  description: "You can do anything with Swotch! Let's dive into dreamy world.",
-  category: categoryArr[2],
-  image: "/img/video_game_console.png"
-}];
+const additionalProducts = [
+  {
+    id: 21,
+    title: "Swotch || Mintendo",
+    price: 389.99,
+    description: "You can do anything with Swotch! Let's dive into dreamy world.",
+    category: categoryArr[2],
+    image: "/img/video_game_console.png"
+  },
+  {
+    id: 22,
+    title: "BOSS Lightweight On-Ear Wired Headphones",
+    price: 199.99,
+    description: "Lightweight headphones in Black with comfortable on-ear design that minimizes outside noise.",
+    category: categoryArr[2],
+    image: "/img/boss-headphone.jpg"
+  },
+  {
+    id: 23,
+    title: "Garmon sports smartwatch",
+    price: 99.99,
+    description: "Full Touch Fitness Tracker IP68 Waterproof Bluetooth Round Sports Smartwatch with Heart Rate Monitor Sleep Monitor Pedometer Activity Tracker Message Notification.",
+    category: categoryArr[2],
+    image: "/img/garmon-smartwatch.jpg"
+  },
+  {
+    id: 24,
+    title: "Roy Ben sunglasses",
+    price: 100.00,
+    description: "Plastic lens, Non-Polarized, 100% UV protection coating.",
+    category: categoryArr[1],
+    image: "/img/roy-ben-sunglasses.jpg"
+  },
+  {
+    id: 25,
+    title: "Orange smart phone 256GB",
+    price: 1200.00,
+    description: `6.53" Large Full Screen&Ultra Wide AI Triple Camera] The 20:9 aspect ratio 6.53" Large Full-Screen display provides a wide field of vision. With the in-cell technology.`,
+    category: categoryArr[2],
+    image: "/img/orange-smartphone.jpg"
+  },
+  {
+    id: 26,
+    title: "Mojo women t-shirt",
+    price: 35.00,
+    description: "95% Polyester + 5% spandex, Super Soft and Comfy.",
+    category: categoryArr[0],
+    image: "/img/mojo-tshirt.jpg"
+  },
+  {
+    id: 27,
+    title: "Dryson hair dryer",
+    price: 250.00,
+    description: "Foldable, lightweight travel hair dryer makes it quick and easy to dry and style at home, work, the gym and on the go.",
+    category: categoryArr[2],
+    image: "/img/dryson-hair-dryer.jpg"
+  },
+  {
+    id: 28,
+    title: "Leather Jackets",
+    price: 150.00,
+    description: "100% Original Premium Lambskin Black Leather Jackets, Zipper closure, Dry Clean.",
+    category: categoryArr[0],
+    image: "/img/leather-jacket.jpg"
+  },
+  {
+    id: 29,
+    title: "Amber drop earrings",
+    price: 200.00,
+    description: "Amber Heart is well poslished, smooth and pleasant to wear near body..",
+    category: categoryArr[1],
+    image: "/img/amber-ear-ring.jpg"
+  }
+];
 
-export { categoryArr as default, additionalProducts};
+export { categoryArr as default, additionalProducts };
