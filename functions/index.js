b16aff506d9e5a368b453851c8b3775881aaa36a
Taichi I <fng.trf.kk1020@gmail.com>
2021-09-20 17:03:13 -0700

    installed cloud functions

--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,33 @@ const functions = require("firebase-functions");
 
 // const stripe = require('stripe')(sk_test_51JPt5UBD2W5bE4pxbgB3SdiuJDhuA7YYyTRBACn9Ginr3wPcpWY2m9bl2ZulylLRaW5DMdghdh5BBBiQKsUHInAo006iYJm3o7);
 
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!!!!", { structuredData: true });
-  response.send("Hello from Firebase!!!");
+  response.send("Hello Taichi!");
 });
-// console.log('Hello from Firebase!!!')
 
 // exports.createPaymentSession = functions.https.onCall(async (data, context) => {
-//   const session = await stripe.checkout.sessions.create
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ['card'],
+//     line_items: [{
+//       price_data: {
+//         currency: 'cad',
+//         product_data: {
+//           name: 'product'
+//         },
+//         unit_amount: 500
+//       },
+//       quantity: 1
+//     }],
+//     mode: 'payment',
+//     success_url: 'http://localhost:3000/payment_success',
+//     cancel_url: 'http://localhost:3000/payment_cancel',
+//   });
+
+//   const res = session;
+//   console.log(res);
+//   return res;
 // })
